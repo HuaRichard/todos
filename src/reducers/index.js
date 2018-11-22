5890c301dfbf73fee7f245416479b3e3ed6335cb
Richard Hua <huachao0349@gmail.com>
2018-11-23 00:03:36 +1100

    add combineReducers without redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,15 @@
-import { combineReducers } from 'redux'
+// import { combineReducers } from 'redux'
 import todos from './todos'
 import visibilityFilter from './visibilityFilter'
 
-export default combineReducers({
-  todos,
-  visibilityFilter
-})
+// export default combineReducers({
+//   todos,
+//   visibilityFilter
+// })
+
+export default function combineReducers(state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    visibilityFilter: visibilityFilter(state.visibiltyFilter, action)
+  }
+}
